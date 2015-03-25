0eea181f42ce449f94706ea7f84702aaa0d5c586
Julia Nething <cjnething@gmail.com>
2015-03-24 19:54:14 -0700

    (feat) Add basic routes to userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,14 +1,58 @@
 /*==================== NOT CURRENTLY IN USE ====================*/
 
 /*==================== REQUIRE DEPENDENCIES ====================*/
-var userController = require('../controllers/userController.js');
+//var userController = require('../controllers/userController.js');
 
 /*=================== SET HANDLERS TO ROUTES ===================*/
-module.exports = function (app) {
-  app.post('/signup', userController.signup);
-  app.post('/login', userController.login);
-  app.post('/logout', userController.logout);
+module.exports = function (app, passport) {
+  //app.post('/signup', userController.signup);
+  //app.post('/login', userController.login);
+  //app.post('/logout', userController.logout);
+
+
+  //HOME PAGE (with login links)
+  app.get('/', function(req, res) {
+    res.render('../client/src/js/landing.html'); //load the landing page
+  });
+
+  //LOGIN
+  //show the login form
+  app.get('/login', function(req, res) {
+    //render the page and pass in any flash data if it exists
+    res.render('../client/src/js/login.html', {message: req.flash('loginMessage')}
+  });
+)};
+
+  //prrocess the login form
+  //app.post('/login', do all the passport stuff here);
+
+
+  //PROFILE SECTION
+  //we will want this protected so you have to be logged in to visit
+  //we will use route middleware to verify this (the isLoggedIn function)
+  app.get('/profile', isLoggedIn, function(req, res) {
+    res.render('../client/src/js/index.html', {
+        user: req.user //get the user out of session and pass to template
+      });
+  });
+
+  //LOGOUT
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
 };
+
+//route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+  //if user is authenticated in the session, carry on
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  //if they aren't redirect them to the home page
+  res.redirect('/');
+}
 
 
 
@@ -67,4 +111,3 @@ module.exports = function (app) {
   // res.redirect('/');
   // });
   
-};
