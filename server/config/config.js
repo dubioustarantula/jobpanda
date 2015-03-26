f1719bee5aa7934b80792f83fbd50189c61753c5
lpww <t.treffry@gmail.com>
2015-03-26 13:53:39 -0700

    (fix)Connection options to jawsdb

--- server/config/config.js
+++ server/config/config.js
@@ -11,14 +11,17 @@ var path = require('path');
 
 /*====== INITIALIZE BOOKSHELF CONNECTION TO POSTGRESS DB ======*/
 
+console.log(process.env.JAWSDB_HOST);
+
 var knex = require('knex')({
 	client: 'mysql',
 	connection: {
-		host: process.env.JAWSDB_URL || 'localhost'/*Local Host for testing, ENV host for deployment*/,
-    // port: process.env.JAWSDB_PORT || 8000,
-		// user: process.env.JAWSDB_USER || 'root',
-		// password: process.env.JAWSDB_PW || '',
-		database: process.env.JAWSDB_DB || 'jobpanda',
+    url: process.env.JAWSDB_URL,
+		// host: process.env.JAWSDB_HOST, //|| 'localhost'/*Local Host for testing, ENV host for deployment*/,
+		user: process.env.JAWSDB_USER ,//|| 'root',
+		password: process.env.JAWSDB_PW, //|| '',
+		database: process.env.JAWSDB_DB ,//|| 'jobpanda',
+    port: process.env.JAWSDB_PORT,
 		charset: 'utf8'
 	}
 });
